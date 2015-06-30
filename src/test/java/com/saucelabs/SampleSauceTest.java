d0f7597e2aac1a0a65ab26f43134162c0d81df20
kristiansl <kristianmeier@kristians-MacBook-Air.local>
2015-06-30 10:58:39 -0700

    adding all the browsers

--- src/test/java/com/saucelabs/SampleSauceTest.java
+++ src/test/java/com/saucelabs/SampleSauceTest.java
@@ -108,16 +108,16 @@ public class SampleSauceTest implements SauceOnDemandSessionIdProvider {
     public static LinkedList browsersStrings() {
         LinkedList browsers = new LinkedList();
         browsers.add(new String[]{"Windows 8.1", "11", "internet explorer", "Windows 8.1 IE 11"}); 
-       // browsers.add(new String[]{"Windows 7", "10", "internet explorer", "Windows 7 IE 10"});   
-       // browsers.add(new String[]{"Windows XP", "42", "chrome", "Windows XP Chrome 42"});   
-       // browsers.add(new String[]{"Windows XP", "36", "firefox", "Windows 8 Firefox 36"});      
-       // browsers.add(new String[]{"OSX 10.8", "6", "safari", "Mac 10.8 Safari 6"});
-       // browsers.add(new String[]{"Linux", "4.4", "Android", "Android Emulator 4.4"}); 
-       // browsers.add(new String[]{"OSX 10.10", "8.2", "iPhone", "iPhone Emulator 8.2"});
-       // browsers.add(new String[]{"Windows 8.1", "11", "internet explorer", "Windows 8.1 IE 11"}); 
-       // browsers.add(new String[]{"Windows 7", "10", "internet explorer", "Windows 7 IE 10"});   
-       // browsers.add(new String[]{"Windows XP", "42", "chrome", "Windows XP Chrome 42"});   
-       // browsers.add(new String[]{"Windows 8", "36", "firefox", "Windows 8 Firefox 36"});
+        browsers.add(new String[]{"Windows 7", "10", "internet explorer", "Windows 7 IE 10"});   
+        browsers.add(new String[]{"Windows XP", "42", "chrome", "Windows XP Chrome 42"});   
+        browsers.add(new String[]{"Windows XP", "36", "firefox", "Windows 8 Firefox 36"});      
+        browsers.add(new String[]{"OSX 10.8", "6", "safari", "Mac 10.8 Safari 6"});
+        browsers.add(new String[]{"Linux", "4.4", "Android", "Android Emulator 4.4"}); 
+        browsers.add(new String[]{"OSX 10.10", "8.2", "iPhone", "iPhone Emulator 8.2"});
+        browsers.add(new String[]{"Windows 8.1", "11", "internet explorer", "Windows 8.1 IE 11"}); 
+        browsers.add(new String[]{"Windows 7", "10", "internet explorer", "Windows 7 IE 10"});   
+        browsers.add(new String[]{"Windows XP", "42", "chrome", "Windows XP Chrome 42"});   
+        browsers.add(new String[]{"Windows 8", "36", "firefox", "Windows 8 Firefox 36"});
  
         return browsers;
     }
